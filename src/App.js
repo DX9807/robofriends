f7724d590ea9cad836ae884866382f015ef08f42
DX9807 <sub.yadav19@gmail.com>
2020-06-14 11:27:44 +0530

    first commit of robofriends app designed using react.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Cardlist from './Cardlist';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+  constructor() {
+    super ();
+    this.state={
+      robots: [],
+      searchField:''
+    }
+    }
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response =>response.json())
+  .then(users=>this.setState({robots: users}))
+  // var request = new XMLHttpRequest();
+  //
+  // // request.onreadystatechange = function () {
+  // //   if (request.readyState==4 && request.status==200){
+  // //     console.log(request.responseText)
+  // //     this.setState({robots: request.responseText})
+  // //   }
+  // //     }
+  //
+  // var URL = "https://jsonplaceholder.typicode.com/users";
+  // request.open('GET',URL,false);
+  // request.send();
+  // console.log(request.responseText)
+  // this.setState({robots: [request.responseText]})
+  }
+
+
+
+  onSearchChange=(event)=>{
+    this.setState({searchField: event.target.value})
+   }
+  render(){
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+    return(
+      <div className="tc">
+        <h2>RoboFriends</h2>
+
+
+        <SearchBox searchChange={this.onSearchChange}/>
+
+        <Scroll>
+        <Cardlist robots={filteredRobots}/>
+        </Scroll>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
